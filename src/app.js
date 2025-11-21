4c71e103ce27d9fab7e28b706daed3ad52f6ebac
rohitkr5850 <rroy854100@gmail.com>
2025-11-22 02:07:37 +0530

    backend

--- src/app.js
+++ src/app.js
@@ -8,32 +8,12 @@ import { apiLimiter } from "./middlewares/rateLimiter.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      const allowed = [
-        "http://localhost:5173",     // local dev
-        "http://127.0.0.1:5173"
-      ];
+app.use(cors({
+  origin: "https://one-india-frontend.vercel.app",
+  methods: ["GET", "POST", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+}));
 
-      const vercel = /\.vercel\.app$/;   // allow ANY Vercel frontend URL
-
-      // allow requests with no origin (like POSTMAN)
-      if (!origin) return callback(null, true);
-
-      if (allowed.includes(origin) || vercel.test(origin)) {
-        callback(null, true);
-      } else {
-        console.log("❌ CORS Blocked:", origin);
-        callback(new Error("CORS Not Allowed"));
-      }
-    },
-
-    methods: ["GET", "POST", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
-  })
-);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
